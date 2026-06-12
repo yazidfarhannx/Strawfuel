1e5b7af016ddde84f7a9aa08fbd53f23cbe8c96e
yazidfarhannx <yazidfarhan226@student.uns.ac.id>
2026-06-12 22:09:39 +0700

    tes otp

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,14 +1,14 @@
 const nodemailer = require('nodemailer');
 
-const transporter =
-  nodemailer.createTransport({
-    service: 'gmail',
-
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS, // HARUS APP PASSWORD
+  },
+});
 
 const sendEmail = async (
   to,
