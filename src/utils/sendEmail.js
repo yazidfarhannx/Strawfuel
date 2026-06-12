dc3f83651b0e60a4aab52f7457a7de2232d35ba0
yazidfarhannx <yazidfarhan226@student.uns.ac.id>
2026-06-12 22:26:22 +0700

    debug lagi

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,15 +2,12 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS, // HARUS APP PASSWORD
+    pass: process.env.SMTP_PASS,
   },
-    tls: {
-    rejectUnauthorized: false
-  }
 });
 
 console.log("SMTP USER:", process.env.SMTP_USER);
